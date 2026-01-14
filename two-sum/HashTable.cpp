8a442f5b0a92d77c19cd08cec73936464d767d99
fei <yoinspiration@outlook.com>
2026-01-14 10:46:06 +0800

    fix: 添加必要的头文件和命名空间
    - 添加 #include <unordered_map> 头文件
    - 添加 #include <vector> 头文件
    - 添加 using namespace std; 声明
    - 修复 'vector is not a template' 编译错误

--- two-sum/HashTable.cpp
+++ two-sum/HashTable.cpp
@@ -1,9 +1,14 @@
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
 class Solution
 {
 public:
   /**
    * @brief 找到数组中两数之和等于目标值的索引
-   * 
+   *
    * @param nums 输入的整数数组
    * @param target 目标值
    * @return vector<int> 包含两个索引的向量，如果未找到则返回空向量
@@ -15,13 +20,13 @@ public:
     for (int i = 0; i < nums.size(); i++)
     {
       int complement = target - nums[i];
-      
+
       // 如果 complement 已经在 map 中，说明找到了
       if (map.find(complement) != map.end())
       {
         return {map[complement], i};
       }
-      
+
       // 将当前元素及其索引存入 map
       map[nums[i]] = i;
     }
